07e8f6bf78a8525fdfc22f320347775fcc1a6721
Dreamer <christopherlu.pdx@gmail.com>
2019-11-15 14:48:21 -0800

    oneRow draws a single row correctly

--- drawX.cpp
+++ drawX.cpp
@@ -5,17 +5,17 @@
 
 using namespace std;
 
-int oneRow(string&, int, char='X');
+int oneRow(string&, int, char = 'X');
 void intro(int&, char&);
 int requestNum(int& indent);
 char requestShape(string& shape);
 
 int main()
 {
-  int i, indent;
+  int i, indent,left, right;
   string c, s, oneRowString;
   char charC;
-
+    
 cout << "intro" << endl;
 
 
@@ -23,42 +23,39 @@ while (indent != 0)
 {
   //request an number
   requestNum(indent);
-
   //request a char
   requestShape(c);
+
+  //cast c as a char to pass into oneRow
+  charC = c[0];
+    
   //blanks the string right before execution of each loop
-  oneRowString = "";
+  s = "";
 
   //for initialize a string the length of indent
   for (i = 0; i < indent; i++)
     {
-   oneRowString.insert(0, "."); 
-    }  
+   s.insert(0, "."); 
+    }
 
- //insert mark character in left and right place
-  for (i = 0; i < indent; i++)
-  {
-    int left = 0;
-    int right = indent - 1;
-  oneRowString.replace(left, 1, c);
-  oneRowString.replace(right, 1, c); 
-//  ++left;
-//  --right;
-  
-
-   cout << oneRowString << endl;
-   }
-  }
-
+   oneRow(s, indent, charC);
+}
   return 0;
 }
 
+//does oneRow does the string modification (what if this int is not indent?)
 int oneRow( string& s, int indent, char c)
 {
-  int i;
- cout << "ping" << endl; 
-//produce a str that is indent spaces long.
-//for (i = 0; i < indent; i++)
+  int left = 0;
+  int right = indent - 1;
+  
+  string stringC;
+  stringC.insert(0, 1, c);
+//really tempted to throw in a 4loop here to increment+decrement and print for ln of indent
+ //replaces char on left/right
+  s.replace(left, 1, stringC);
+  s.replace(right, 1, stringC);
+  cout << s << endl;
 
   return 0;
 }
